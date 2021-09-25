9a6c8bdae3fa88f45a70272a31d6470b2438af3e
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-09-25 21:06:36 +0900

    cors 오류 해결 기능 추가

--- back/index.js
+++ back/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable consistent-return */
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const morgan = require('morgan');
 
 const app = express();
 const port = 5000;
@@ -18,6 +22,23 @@ mongoose.connect(`${process.env.mongoURI}`)
         console.log(err);
     });
 
+// CORS 문제 해결
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+  app.use(morgan('combined'));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
+  app.use(cors({
+    origin: '', // 배포된 프론트 서버 주소
+    credentials: true,
+  }));
+} else {
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
